d112e7713ee7a3abefb9ab6076e258b220200c78
Emeka Onyebuchi <chybyke.emeka@gmail.com>
2019-12-09 00:09:19 +0000

    writing tests

--- runTests.py
+++ runTests.py
@@ -1,5 +1,5 @@
 
-from String_Engine_API import getErrorStatements
+from String_Engine_API import getErrorStatements,createUniqueList,findIndex,sortList
 
 def test_error_functions():
     assert getErrorStatements(1) == "You have entered a wrong format. Kindly retry request using string format"
@@ -9,6 +9,17 @@ def test_error_functions():
     assert getErrorStatements(3) == "You have gone to the wrong route, kindly visit /permutation-index"
     assert getErrorStatements(4) == "Something went wrong, please contact the administrator"
 
+def test_createUniqueList():
+    assert createUniqueList(['abc','abc','abc']) == ['abc']
+
+def test_findIndex():
+    assert findIndex('a',['a','b','c']) == 0
+def test_sortList():
+    assert sortList(['z','y','x']) == ['x','y','z']
+
 if __name__ == "__main__":
     test_error_functions()
+    test_createUniqueList()
+    test_findIndex()
+    test_sortList
     print("All tests passed")
